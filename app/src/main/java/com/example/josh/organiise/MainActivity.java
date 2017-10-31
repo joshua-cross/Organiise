2872007074a5e3bbd4cdcffd2245479b36f0daa9
Josh Cross <crossjoshua6@gmail.com>
2017-10-31 12:22:12 +0000

    Application now sends messages without the need for the button press every 10 seconds using a Java Timer.

--- app/src/main/java/com/example/josh/organiise/MainActivity.java
+++ app/src/main/java/com/example/josh/organiise/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.josh.organiise;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -27,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        startCountdown();
+
         //creating a reference to the ACTION input.
         action = (EditText) findViewById(R.id.currentAction);
         //reference to the button which will submit the action.
@@ -38,6 +43,35 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
+    //TO-DO: need a timer for every day/week/month to show the pie chart, this will need a new notification menu aswell...
+
+    //timer that shows the user every hour that they need to input new data.
+    public void startCountdown()
+    {
+
+        //int millis = ((hours*60)+mins)*60000; // Need milliseconds to use Timer
+        int millis = 10000;
+
+        new Timer().schedule(new TimerTask()
+        {
+            @Override
+            public void run()
+            {
+                //code that runs when timer is done
+                showNotification();
+                //calling a method to reset the timer.
+                resetTimer();
+            }
+        }, millis);
+    }
+
+    //Method thats only job is to recall startCountdown after the timer has finished.
+    private void resetTimer() {
+        startCountdown();
+    }
+
 
     //function that prints the notifications, this will be printed every hour.
     public void showNotification() {
---
+++ app/src/main/java/com/example/josh/organiise/NotificationPublisher.java
@@ -0,0 +1,29 @@
+package com.example.josh.organiise;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by josh on 30/10/2017.
+ */
+
+public class NotificationPublisher extends BroadcastReceiver {
+
+    public static String NOTIFICATION_ID = "notification-id";
+    public static String NOTIFICATION = "notification";
+
+    public void onReceive(Context context, Intent intent) {
+
+        NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        Notification notification = intent.getParcelableExtra(NOTIFICATION);
+        int id = intent.getIntExtra(NOTIFICATION_ID, 0);
+        notificationManager.notify(id, notification);
+
+    }
+}
